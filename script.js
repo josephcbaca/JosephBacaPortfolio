c6970507109d46d33fcbd470d0950c158557cac0
Joseph Baca <josephcbaca@gmail.com>
2020-07-29 22:41:17 -0700

    Completed copy email to clipboard functionality.

--- script.js
+++ script.js
@@ -2,28 +2,17 @@ $('.flip').hover(function () {
     $(this).find('.card').toggleClass('darkened-image');
 });
 
-$("#copy-text").on("click", function (event) {
-    event.preventDefault();
+$("#copy-text").on("click", function () {
 
-    let emailText = "josephcbaca@gmail.com"
-    
-    emailText.select();
-    // copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    copyToClipBoard("josephcbaca@gmail.com");
+});
 
+const copyToClipBoard = (str) =>
+{
+    const el = document.createElement("textarea");
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
     document.execCommand("copy");
-})
-
-// function myFunction() {
-//   /* Get the text field */
-//   let copyText = document.getElementById("myInput");
-
-//   /* Select the text field */
-//   copyText.select();
-//   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-//   /* Copy the text inside the text field */
-//   document.execCommand("copy");
-
-//   /* Alert the copied text */
-//   alert("Copied the text: " + copyText.value);
-// }
+    document.body.removeChild(el);
+};
