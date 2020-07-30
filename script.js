e9671fcc2d5351752c46b6eba2639dfadf5316fb
Joseph Baca <josephcbaca@gmail.com>
2020-07-29 23:52:26 -0700

    Added pop up letting user know email has been copied.

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ $('.flip').hover(function () {
 });
 
 $("#copy-text").on("click", function () {
-
     copyToClipBoard("josephcbaca@gmail.com");
+    popUpClipBoard()
 });
 
-const copyToClipBoard = (str) =>
-{
+const copyToClipBoard = (str) => {
     const el = document.createElement("textarea");
     el.value = str;
     document.body.appendChild(el);
@@ -16,3 +15,11 @@ const copyToClipBoard = (str) =>
     document.execCommand("copy");
     document.body.removeChild(el);
 };
+
+function popUpClipBoard() {
+    $("#popUp").show();
+    setTimeout(function () {
+        $("#popUp").hide();
+    }, 300);
+
+  }
