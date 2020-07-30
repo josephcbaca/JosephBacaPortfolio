bcfe5ba3ed1497abeb9e8c44c2670f7a822a200a
Joseph Baca <josephcbaca@gmail.com>
2020-07-29 18:36:35 -0700

    Updates to portfolio page to footer navbar.

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-$('.flip').hover(function(){
+$('.flip').hover(function () {
     $(this).find('.card').toggleClass('darkened-image');
-
 });
+
+$("#copy-text").on("click", function (event) {
+    event.preventDefault();
+
+    let emailText = "josephcbaca@gmail.com"
+    
+    emailText.select();
+    // copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    document.execCommand("copy");
+})
+
+// function myFunction() {
+//   /* Get the text field */
+//   let copyText = document.getElementById("myInput");
+
+//   /* Select the text field */
+//   copyText.select();
+//   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+//   /* Copy the text inside the text field */
+//   document.execCommand("copy");
+
+//   /* Alert the copied text */
+//   alert("Copied the text: " + copyText.value);
+// }
