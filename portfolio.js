b60f5c92274506dec959b2b23b754ce6a7ffb2f5
Joseph Baca <josephcbaca@gmail.com>
2020-09-22 22:26:38 -0700

    Added The Golf Club to Projects.

--- portfolio.js
+++ portfolio.js
@@ -30,34 +30,41 @@ function popUpClipBoard() {
 let projects = [
     {
         id: 1,
+        img: "./assets/projects/TheGolfClub.jpg",
+        alt: "Screenshot of The Golf Club project.",
+        h5: "The Golf Club",
+        siteHref: "https://uwbc-golfclub.herokuapp.com/",
+        ghHref: "https://github.com/josephcbaca/golfclub"
+    }, {
+        id: 2,
         img: "./assets/projects/brewr.jpg",
         alt: "Screenshot of brewR project.",
         h5: "brewR",
         siteHref: "https://brewrs.herokuapp.com/",
         ghHref: "https://github.com/JMantis0/brewR"
     }, {
-        id: 2,
+        id: 3,
         img: "./assets/projects/DecisionsDecisions.jpg",
         alt: "Screenshot of Decisions Decisions project.",
         h5: "Decisions Decisions",
         siteHref: "https://josephcbaca.github.io/DecisionsDecisions/",
         ghHref: "https://github.com/josephcbaca/DecisionsDecisions"
     }, {
-        id: 3,
+        id: 4,
         img: "./assets/projects/burgermeister.jpg",
         alt: "Screenshot of The Burgermeister project.",
         h5: "The Burgermeister",
         siteHref: "https://immense-thicket-40150.herokuapp.com/",
         ghHref: "https://github.com/josephcbaca/Burgermeisterz-Revenge"
     }, {
-        id: 4,
+        id: 5,
         img: "./assets/projects/DayPlanner.jpg",
         alt: "Screenshot of Personal Day Planner project.",
         h5: "Personal Day Planner",
         siteHref: "https://josephcbaca.github.io/PersonalDayPlanner/",
         ghHref: "https://github.com/josephcbaca/PersonalDayPlanner"
     }, {
-        id: 5,
+        id: 6,
         img: "./assets/projects/WeatherDashboard.jpg",
         alt: "Screenshot of Weather Dashboard project.",
         h5: "Weather Dashboard",
